7d6dea133fc9466cbcce4925e64d0bb534a08ecc
gurjyan <gurjyan@jlab.org>
2020-06-16 19:39:11 -0400

    optimizing...

--- src/VtpListeningServer.java
+++ src/VtpListeningServer.java
@@ -125,22 +125,23 @@ public class VtpListeningServer {
             long ts_sec = Long.reverseBytes(dataInputStream.readLong());
             long ts_nsec = Long.reverseBytes(dataInputStream.readLong());
 
-            System.out.println("source_id         = " + Long.toHexString(source_id));
+            System.out.println("source_id         = " + Integer.toHexString(source_id));
             System.out.println("total_length      = " + total_length);
             System.out.println("payload_length    = " + payload_length);
             System.out.println("compressed_length = " + compressed_length);
-            System.out.println("magic             = " + Long.toHexString(magic));
-            System.out.println("format_version    = " + Long.toHexString(format_version));
+            System.out.println("magic             = " + Integer.toHexString(magic));
+            System.out.println("format_version    = " + Integer.toHexString(format_version));
             System.out.println("record_number     = " + record_number);
             System.out.println("ts_sec            = " + ts_sec);
             System.out.println("ts_nsec           = " + ts_nsec);
 
 
-            byte[] dataBuffer = new byte[(int)(total_length) - (12 * 4)];
+            byte[] dataBuffer = new byte[total_length - (12 * 4)];
                 dataInputStream.readFully(dataBuffer);
 
         } catch (IOException e) {
             e.printStackTrace();
+            System.exit(1);
         }
 
     }
