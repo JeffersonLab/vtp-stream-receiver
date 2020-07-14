8ab22e436e0038b69c901bd286867d4009f75081
gurjyan <gurjyan@jlab.org>
2020-07-14 15:34:03 -0400

    hardware version

--- src/VtpListeningServer.java
+++ src/VtpListeningServer.java
@@ -212,7 +212,7 @@ public class VtpListeningServer {
             int slot_ind = Utility.getUnsignedShort(bb);
             int slot_len = Utility.getUnsignedShort(bb);
             if (slot_ind > 0 && slot_len > 0) {
-                for (int i = slot_ind * 4; i < slot_len / 4; i++) {
+                for (int i = slot_ind * 4; i < (slot_len -1) / 4; i++) {
 //                    System.out.println("slot_ind= " + slot_ind + " slot_len= " + slot_len + " " + payload.length + " limit= " + bb.limit());
                     long payload_data_point = Utility.getUnsignedInt(bb);
                 }
