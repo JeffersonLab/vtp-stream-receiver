c4ff8939dc5ebc24d52e9f26adc140cdbc4b1664
gurjyan <gurjyan@jlab.org>
2020-07-15 10:45:07 -0400

    full payload decoding

--- src/org/jlab/epsci/ersap/services/receivers/StreamReceiver.java
+++ src/org/jlab/epsci/ersap/services/receivers/StreamReceiver.java
@@ -158,7 +158,8 @@ public class StreamReceiver {
                         type = (payload_data_point >> 15) & 0xFFFF;
                         rocid = (payload_data_point >> 8) & 0x007F;
                         slot = (payload_data_point) & 0x001F;
-                    }else if (type == 0x0001) /* FADC hit type */ {
+                    }
+                    if (type == 0x0001) /* FADC hit type */ {
                         q = (payload_data_point) & 0x1FFF;
                         ch = (payload_data_point >> 13) & 0x000F;
                         t = ((payload_data_point >> 17) & 0x3FFF) * 4;
