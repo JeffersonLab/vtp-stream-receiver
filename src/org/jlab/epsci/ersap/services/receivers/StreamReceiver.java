e1077874b39bf84e8aa1ade7f4f0c870dbd6839c
gurjyan <gurjyan@jlab.org>
2020-07-15 23:10:51 -0400

    debugging...

--- src/org/jlab/epsci/ersap/services/receivers/StreamReceiver.java
+++ src/org/jlab/epsci/ersap/services/receivers/StreamReceiver.java
@@ -164,7 +164,7 @@ public class StreamReceiver {
                         type = (payload_data_point >> 15) & 0xFFFF;
                         rocid = (payload_data_point >> 8) & 0x007F;
                         slot = (payload_data_point) & 0x001F;
-//                            System.out.println("type = " + type + " rocid = " + rocid + " slot = " + slot);
+                            System.out.println("type = " + type + " rocid = " + rocid + " slot = " + slot);
                     } else if (type == 0x0001) /* FADC hit type */ {
                         q = (payload_data_point) & 0x1FFF;
                         ch = (payload_data_point >> 13) & 0x000F;
@@ -173,7 +173,7 @@ public class StreamReceiver {
                 }
             }
         }
-//        System.out.println();
+        System.out.println();
 //    }
     }
 
